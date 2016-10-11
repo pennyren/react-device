577096c1e66e712f5f2916e5859266e4f19c9ca9
Penny Ren <emotionalecho@gmail.com>
2016-10-11 22:41:57 +0800

    complete checkbox

--- app/components/Checkbox/index.js
+++ app/components/Checkbox/index.js
@@ -3,11 +3,19 @@ import Ripple from 'components/Ripple';
 import styles from './styles.css';
 
 class Checkbox extends Component {
-	toggle = (e) => {
+	toggle = () => {
+		if (this.checkbox.classList.contains('disabled')) {
+			return;
+		}
+		const isChecked = this.rawCheck.checked;
+		const onChange = this.props.onChange;
 		this.checkbox.classList.toggle('checked');
+		this.rawCheck.checked = !isChecked;
+		typeof onChange == 'function' && onChange(!isChecked)
 	}
 
 	render() {
+		const {name, label} = this.props;
 		return (
 			<div className="checkbox-wrapper">
 				<div className="checkbox" ref={r => this.checkbox = r} onClick={this.toggle}>
@@ -15,11 +23,10 @@ class Checkbox extends Component {
 					<div className="inner">
 						<i className="mdi mdi-check"></i>
 					</div>
-					<input type="checkbox"/>
+					<input type="checkbox" name={name} ref={r => this.rawCheck = r}/>
 					<Ripple centerRipple={true} color="#b4c5cd"/>
 				</div>
-				<div className="label">
-				</div>
+				<label>{label}</label>
 			</div>
 		);
 	}
