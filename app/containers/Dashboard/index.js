bdf657ea6e8a4faed6bc971b239a5b1b9a3a4f32
Princess310 <837478514@qq.com>
2016-10-14 02:31:04 +0800

    . add debug grid system for app

--- app/containers/Dashboard/index.js
+++ app/containers/Dashboard/index.js
@@ -1,50 +1,51 @@
 import React, {Component} from 'react';
 import AppBar from 'components/AppBar';
+import Debug from 'components/Debug';
 import styles from './styles.css';
 
 class Dashboard extends Component {
 	componentWillUnmount() {
-        clearTimeout(this.enterTimer);
-        clearTimeout(this.leaveTimer);
-    }
+		clearTimeout(this.enterTimer);
+		clearTimeout(this.leaveTimer);
+	}
 
-    componentWillAppear(callback) {
-        this.initAnimation(callback);
-    }
+	componentWillAppear(callback) {
+		this.initAnimation(callback);
+	}
 
-    componentWillEnter(callback) {
-        this.initAnimation(callback);
-    }
+	componentWillEnter(callback) {
+		this.initAnimation(callback);
+	}
 
-    componentDidAppear() {
-        this.animate();
-    }
+	componentDidAppear() {
+		this.animate();
+	}
 
-    componentDidEnter() {
-        this.animate();
-    }
+	componentDidEnter() {
+		this.animate();
+	}
 
-    componentWillLeave(callback) {
-        const style = this.dashboard.style;
-        style.opacity = 0;
-        console.log(2);
-        this.leaveTimer = setTimeout(callback, 0);
-    }
+	componentWillLeave(callback) {
+		const style = this.dashboard.style;
+		style.opacity = 0;
+		console.log(2);
+		this.leaveTimer = setTimeout(callback, 0);
+	}
 
-    animate() {
-        const style = this.dashboard.style;
-        const opacity = 'opacity 2000ms cubic-bezier(0.23, 1, 0.32, 1) 0ms';
-       
-        style.opacity = 1;
-        style.transition = opacity;
-    }
+	animate() {
+		const style = this.dashboard.style;
+		const opacity = 'opacity 2000ms cubic-bezier(0.23, 1, 0.32, 1) 0ms';
+	   
+		style.opacity = 1;
+		style.transition = opacity;
+	}
 
-    initAnimation(callback) {
-        const style = this.dashboard.style;
-        style.opacity = 0;
-        
-        this.enterTimer = setTimeout(callback, 0);
-    }
+	initAnimation(callback) {
+		const style = this.dashboard.style;
+		style.opacity = 0;
+		
+		this.enterTimer = setTimeout(callback, 0);
+	}
 	render() {
 		return (
 			<div className="dashboard" ref={r => this.dashboard =r}>
@@ -55,6 +56,7 @@ class Dashboard extends Component {
 						{this.props.children}
 					</div>
 				</div>
+				<Debug />
 			</div>
 		)
 	}
