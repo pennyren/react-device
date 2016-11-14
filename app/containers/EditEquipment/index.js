3309d592650f68dc9f56cd58cfdacc0a54c8d962
Penny Ren <emotionalecho@gmail.com>
2016-11-14 23:00:37 +0800

    add edit equipment

--- app/containers/EditEquipment/index.js
+++ app/containers/EditEquipment/index.js
@@ -8,9 +8,23 @@ class EditEquipment extends Component {
 		
 		return (
 			<div className="edit-equipment">
-				<section className="base-info"></section>
-				<section className="fund-info"></section>
-				<section className="bill-info"></section>
+				<section className="box-info spec">
+					<div className="title">规格参数</div>
+				</section>
+
+				<div className="wrapper">
+					<section className="box-info fund">
+						<div className="title">经费</div>
+					</section>
+
+					<section className="box-info bill">
+						<div className="title">单据</div>
+					</section>
+
+					<section className="box-info other">
+						<div className="title">其它</div>
+					</section>
+				</div>
 			</div>
 		)
 	}
