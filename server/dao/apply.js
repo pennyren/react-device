e8161c45acbed7032faf0923bdbc5062b690d88c
Penny Ren <emotionalecho@gmail.com>
2016-12-01 22:32:26 +0800

    add approval dao

--- server/dao/apply.js
+++ server/dao/apply.js
@@ -6,8 +6,31 @@ class ApplyDao extends BaseDao {
 		super('apply');
 	}
 
-	getOffsetList = async (filter, currentCount) => {
-		
+	getCurrentApproval = async (applyId) => {
+		const sql = `
+			select 
+			a.*, u.username, e.name, e.version
+			from "apply" a
+			left join "user" u on u.id = a."userId" 
+			left join "equipment" e on a."equipmentNumber" = e."serialNumber"
+			where a.id = ${applyId}
+		`;
+		const result = await executeQuery(sql);
+		return result.rows[0];
+	}
+
+	getOffsetApprovals = async (userId, offset) => {
+		const sql = `
+			select 
+			a.id, a."currentApprovalUserId", u.username, a.type, a."equipmentNumber" as number, e.name, e.version, a.ctime
+			from "apply" a
+			left join "user" u on u.id = a."userId" 
+			left join "equipment" e on a."equipmentNumber" = e."serialNumber"
+			where ${userId} = any (a."approvalUserIds")
+			limit 10 offset ${offset}
+		`;
+		const result = await executeQuery(sql);
+		return result.rows;
 	}
 }
 
